53fd279be063568306d45e2780842085d9c9a4fe
Marcus Felipy Glatz Rodrigues <marcus_felipy@hotmail.com>
2021-10-26 10:36:52 -0400

    comentando shows

--- src/OpenDSS.py
+++ src/OpenDSS.py
@@ -26,9 +26,9 @@ circuit.Solution.Solve()
 # Exportando os monitores para formato .csv
 text.Command = f'Export monitors linha1_voltage'
 text.Command = f'Export monitors linha1_power'
-# Comando para mostrar informações relevantes
-text.Command = f'Show voltage ln nodes'
-text.Command = f'Show Powers kva elem'
+# # Comando para mostrar informações relevantes
+# text.Command = f'Show voltage ln nodes'
+# text.Command = f'Show Powers kva elem'
 
 # Pegando informações do medidor_power e adicionando em um Data Frame
 mon_power = pd.read_csv('IEEE13Nodeckt_Mon_linha1_power.csv', sep=',')
